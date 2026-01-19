78e18bb3ce72dba9de4dd2d1c7db41dfb1ab76d7
vidovb <vidovb@taltech.ee>
2026-01-19 18:10:59 +0200

    Add behavior settings for enabling/disabling destructive select into in MSAccess

---
+++ MSAccessBehaviorSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using NppDB.Comm;
+
+namespace NppDB.MSAccess
+{
+    internal static class MsAccessBehaviorSettings
+    {
+        internal static string TryGetSettingsPath(INppDbCommandHost commandHost)
+        {
+            var dir = commandHost?.Execute(NppDbCommandType.GET_PLUGIN_CONFIG_DIRECTORY, null) as string;
+            if (string.IsNullOrWhiteSpace(dir)) return null;
+            return Path.Combine(dir, "behavior_settings.json");
+        }
+
+        internal static bool IsDestructiveSelectIntoEnabled(string settingsPath)
+        {
+            if (string.IsNullOrWhiteSpace(settingsPath)) return false;
+            if (!File.Exists(settingsPath)) return false;
+
+            var json = File.ReadAllText(settingsPath);
+            if (string.IsNullOrWhiteSpace(json)) return false;
+
+            var value = JsonConvert.DeserializeObject<BehaviorSettings>(json);
+            return value.EnableDestructiveSelectInto;
+        }
+
+        private struct BehaviorSettings
+        {
+            public bool EnableDestructiveSelectInto { get; set; }
+        }
+    }
+}
